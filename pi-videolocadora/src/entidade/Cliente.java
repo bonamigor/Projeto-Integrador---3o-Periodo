d7f269b65fe248cf0cfd33df048a364d8743e314
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2019-05-16 22:02:34 -0300

    Commit p/ master

---
+++ pi-videolocadora/src/entidade/Cliente.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidade;
+
+import java.util.Date;
+
+/**
+ *
+ * @author bonamigo
+ */
+public class Cliente {
+    
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+    private String email;
+    private Date data_nascimento;
+
+    public Cliente(Integer id, String nome, String cpf, String telefone, String endereco, String email, Date data_nascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.email = email;
+        this.data_nascimento = data_nascimento;
+    }
+    
+    public Cliente(){
+        
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+    
+}
